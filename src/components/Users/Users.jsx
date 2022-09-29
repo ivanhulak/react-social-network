4e8bf692b39570671ae70083724a4c0d01ae9247
ivanhulak <g.ivan.gulak17@gmail.com>
2022-09-29 23:28:45 +0300

    Create class Component Users.jsx, use life cycle method componentDidMount to paint my users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,30 +3,36 @@ import UserItem from "./UserItem/UserItem";
 import styles from './Users.module.css';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    if(props.users.length === 0){
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
+
+class Users extends React.Component {
+    
+
+    componentDidMount(){
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {this.props.setUsers(response.data.items);
         })
     }
 
-    let userElements = props.users
-        .map(u => <UserItem key={u.id} userId={u.id} userPhoto={u.photos.small}
-                            followed={u.followed}
-                            userName={u.name}
-                            status={u.status}
-                            follow={props.follow}
-                            unfollow={props.unfollow}/>)
+    
 
-    return (
-        <div className={styles.users}>
-            <div>Users</div>
-            <div className={styles.userItemsRow}>
-                {userElements}
+    render() {
+        let userElements = this.props.users
+                .map(u => <UserItem key = { u.id } userId = { u.id } 
+                            userPhoto = { u.photos.small }
+                            followed = { u.followed }
+                            userName = { u.name }
+                            status = { u.status }
+                            follow = { this.props.follow }
+                            unfollow = { this.props.unfollow } />)
+        return (
+            <div className={styles.users}>
+                <div>Users</div>
+                <div className={styles.userItemsRow}>
+                    {userElements}
+                </div>
             </div>
-
-        </div>
-    );
+        );
+    }
 }
 
 export default Users;
