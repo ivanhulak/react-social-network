ac0d403b7ff0b1df6b7ff00c90f572e8c9dd9755
ivanhulak <g.ivan.gulak17@gmail.com>
2022-12-17 14:16:07 +0200

    Deploy my SN to GitHub pages, add HashRouter to avoid problems with routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './reset.css';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
